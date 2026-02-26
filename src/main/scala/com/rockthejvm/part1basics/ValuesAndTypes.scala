8a8712fa511751c6cdde2a2b900a8736b7e1815c
grml <grml@grml.grml>
2026-02-26 13:17:58 +0800

    feat: added values and types object file from video 1

---
+++ src/main/scala/com/rockthejvm/part1basics/ValuesAndTypes.scala
@@ -0,0 +1,32 @@
+package com.rockthejvm.part1basics
+
+object ValuesAndTypes {
+
+  // unlike java, and more similar to modern statically typed language, it is 'val name: dataType = value data'
+  // values are also immutable in scala
+  val meaningOfLife: Int = 42
+
+  // type inference in scala
+  // val greeting: String = "Hello"
+  // val numbers: List[Int] = List(1, 2, 3)
+
+  // can also be expressed as
+
+  // val greeting = "Hello"
+  // val numbers = List(1, 2, 3)
+
+  // common data types
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a' // similar to C, ' is used instead of " for character literals. "A" represents a string object containing 'A' and a hidden string terminator '\0'
+  val anInt: Int = 45 // Int 4 bytes
+  val aShort: Short = 5234 // 2 bytes
+  val aFloat: Float = 2.4f // 4 byte dec
+  val aDouble: Double = 3.13 // 8 byte dec
+  val aLong: Long = 84927498329482L // 8 byte ints, used a lot in ids, like in java
+  val aString: String = "scala rocks"
+
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
